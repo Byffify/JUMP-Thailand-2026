755488fe52a4769d709a0e449afabc8da5304e73
Borworn Kultumyotin <177917303+Byffify@users.noreply.github.com>
2026-08-02 17:39:23 +0700

    Project sturcture

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+import Navbar from "./components/Navbar.jsx";
+import Hero from "./components/Hero.jsx";
+
+function App() {
+  return (
+    <div>
+    </div>
+  );
+}
+
+export default App;
